05d38817a6e848bf9a055e6813110c28e64ca4a6
andres <andresbertuccio@gmail.com>
2020-05-24 23:44:25 -0300

    conectamos con nominatim

--- nominatim_api.js
+++ nominatim_api.js
@@ -1,4 +1,6 @@
 const pgp = require('pg-promise')(/* initialization options */);
+const http = require('http')
+
 
 const cn = {
     host: 'localhost', 
@@ -12,8 +14,33 @@ const db = pgp(cn);
 
 (async()=>{
 
-    const result  = await db.one("select count(*) from rnpr_normalizacion rn");
+    const result  = await db.many("select * from provincias");
     
-    console.log(result)
+    const street=null;
+    const city=null;
+    const county="Morón";
+    const state="Buenos Aires";
+    const country="Argentina";
+    const postalcode=null;
+
+    let output = '';
+
+    http.get({
+        hostname: 'localhost',
+        port: 7070,
+        path: '/search.php?country=Argentina&state=Buenos%20aires&county=Moron&polygon_geojson=1&format=json'
+      }, (res) => {          
+        res.on('data', (chunk) => {
+            output += chunk;
+          });  
+        res.on('end', () => {
+            let obj = JSON.parse(output);
+            console.log(obj) 
+      
+            
+            //onResult(res.statusCode, obj);
+          });
+      });
+
 
 })();
