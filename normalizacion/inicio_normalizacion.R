e1c01cf1cfc448a9792b6de81ca0a43c1a3c5f35
andres <andresbertuccio@gmail.com>
2020-06-01 20:55:06 -0300

    query

--- normalizacion/inicio_normalizacion.R
+++ normalizacion/inicio_normalizacion.R
@@ -8,13 +8,12 @@ origen_paises <- dbGetQuery(con, "select distinct pais as nombre from rnpr_disti
 origen_paises$nombre <- trimws(origen_paises$nombre)
 origen_paises <- data.frame(nombre = origen_paises[origen_paises$nombre != '',])
 norma_paises <- dbGetQuery(con, "select id_pais as id,
-                                        nombre_iso as nombre 
-                                        from paises p
-                                union
-                                select id_pais as id, 
-                                        sinonimo as nombre 
-                                        from rnpr_sinonimos rs 
-                                where codigo in (select codigo_numerico from paises p)")
+                                  nombre
+                                  from paises p
+                                  union 
+                                  select sp.id_pais as id, 
+                                  sp.sinonimo as nombre 
+                                  from sinonimos_paises sp")
 
 paises <- normalizacion(origen_paises,norma_paises)
 
