8ebf3f01a49e7b2facdaa21bc39de1a759076ea1
jlobasso <jlobasso@gmail.com>
2020-07-07 17:34:32 -0300

    pruea conf

--- normalizacion/prueba.R
+++ normalizacion/prueba.R
@@ -8,5 +8,7 @@ if(dbExistsTable(pg_conexion, "jlobasso.prueba001")){
   dbRemoveTable(pg_conexion,"jlobasso.prueba001")
 }
 pruebamem <- dbGetQuery(pg_conexion, "select p2.nombre as provincia, id_provincia FROM smap.provincias p2")
-dbWriteTable(pg_conexion, "jlobasso.prueba001", pruebamem, row.names=TRUE, append=FALSE)
+# dbWriteTable(pg_conexion, "", pruebamem, row.names=TRUE, append=FALSE)
+dbWriteTable(pg_conexion, c("jlobasso","prueba001"), value=pruebamem, row.names=TRUE, append=FALSE)
 dbDisconnect(pg_conexion)
+
