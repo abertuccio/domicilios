9e9449263947dc293bb20bf9ab1a4d2a3d328341
abertuccio <abertuccio@users.noreply.github.com>
2020-08-25 15:02:25 -0300

    Update data_explorer.R

--- normalizacion/utils/data_explorer.R
+++ normalizacion/utils/data_explorer.R
@@ -7,8 +7,8 @@ require("rpostgis")
 
 # con<-dbConnect(dbDriver("PostgreSQL"), dbname = 'domicilios', host='localhost', port=9999, user='postgres', password=1234)
 
-conn <- dbConnect(dbDriver("PostgreSQL"), host = "localhost", dbname = "pgsint" , port=9999 ,
-                  user = "postgres", password = "1234")
+conn <- dbConnect(dbDriver("PostgreSQL"), host = "localhost", dbname = "dbname" , port=0000 ,
+                  user = "postgres", password = "password")
 
 query<-"select point from smap.asentamientos where id_asentamiento = 1114"
 # arne <- pgGetGeom(conn, query=query)
